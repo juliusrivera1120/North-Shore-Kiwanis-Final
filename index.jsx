099dc5cf296c341db9427c9fe812a28f015202d8
juliusrivera1120 <juliusrivera1120@gmail.com>
2025-12-23 22:33:17 -0500

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles.css";
+
+// Get the root element from index.html
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+// Render the app
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
